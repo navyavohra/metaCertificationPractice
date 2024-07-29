841473f33e2c005382b9ad277c934a530ac56f2d
Navya <vohrana@sheridancollege.ca>
2024-07-29 23:03:12 +0530

    Initial Commit

---
+++ metaCertificationPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  metaCertificationPractice
+//
+//  Created by Navya Vohra on 2024-07-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ metaCertificationPractice/metaCertificationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  metaCertificationPracticeApp.swift
+//  metaCertificationPractice
+//
+//  Created by Navya Vohra on 2024-07-29.
+//
+
+import SwiftUI
+
+@main
+struct metaCertificationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
